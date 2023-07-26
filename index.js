f470d6ee0b84c9d899c617811da2a1327a743408
jahnav7 <yendluri_b200847cs@nitc.ac.in>
2023-07-26 12:11:17 +0530

    changed links

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const mongoDB=require("./db")
 mongoDB();
 const cors = require('cors');
 app.use(cors());
-app.use(cors({ origin: 'http://localhost:3000', credentials: true }));
+app.use(cors({ origin: 'https://the-easy-food.netlify.app', credentials: true }));
 {/*app.use((req,res,next)=>{
   res.setHeader("Access-Control-Allow-Origin","http://localhost:3000");
   res.header("Access-Control-Allow-Headers","Origin, X-Requested-With, Content-Type, Accept"
